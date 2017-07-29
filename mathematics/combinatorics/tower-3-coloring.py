4f1b59c52d825d6cecf875925e0e169e94da962a
JulStrat <ioulianos.kakoulidis@hotmail.com>
2017-07-29 13:53:44 +0300

    Update tower-3-coloring.py

--- mathematics/combinatorics/tower-3-coloring.py
+++ mathematics/combinatorics/tower-3-coloring.py
@@ -4,12 +4,12 @@ readl = stdin.readline
 MOD = 10**9 + 7
 
 def towerColoring(n):
-    return pow(3, pow(3, n, MOD - 1), MOD)
+    return(pow(3, pow(3, n, MOD - 1), MOD))
 
 def main():
     n = int(readl())
     r = towerColoring(n)
-    print (r)
+    print(r)
 
 if __name__ == '__main__':
     main()
