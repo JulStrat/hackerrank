ee2fd7c1412e3735f0ed3d553d0e39df2de0fe9b
JulStrat <ioulianos.kakoulidis@hotmail.com>
2017-07-29 14:14:01 +0300

    Update merge-list.py

--- mathematics/combinatorics/merge-list.py
+++ mathematics/combinatorics/merge-list.py
@@ -1,17 +1,24 @@
-#!/bin/python3
-MOD = 10**9 + 7
+'''
+https://www.hackerrank.com/challenges/merge-list
+'''
 
 from functools import reduce
 from operator import mul
 from sys import stdin
 readl = stdin.readline
 
+MOD = 10**9 + 7
+
 def ncr(n, r):
   r = min(r, n - r)
   nom = reduce(mul, range(n, n - r, -1), 1)
-  denom = reduce(mul, range(1, r + 1), 1)
-  return (nom//denom)
+  den = reduce(mul, range(1, r + 1), 1)
+  return(nom//den)
 
-for _ in range(int(readl())):
-  n, m = map(int, readl().split())
-  print (ncr(n + m, n)%MOD)
+def main():
+    for __ in range(int(readl())):
+        n, m = map(int, readl().split())
+        print(ncr(n + m, n)%MOD)
+
+if __name__ == '__main__':
+    main()        
