0544cf6d10419cadc749b5299d1f9c1198d5cf99
JulStrat <ioulianos.kakoulidis@hotmail.com>
2017-07-29 14:35:15 +0300

    Update antipalindromic-strings.py

--- mathematics/combinatorics/antipalindromic-strings.py
+++ mathematics/combinatorics/antipalindromic-strings.py
@@ -6,10 +6,14 @@ readl = stdin.readline
 
 MOD = 10**9 + 7
 
-for __ in range(int(readl())):
-  n, m = map(int, readl().split())
-  r = m
-  if n > 1:
-    r *= m - 1
-    r *= pow(m - 2, n - 2, MOD)
-  print(r%MOD)
+def main():
+    for __ in range(int(readl())):
+        n, m = map(int, readl().split())
+        r = m
+        if n > 1:
+            r *= m - 1
+            r *= pow(m - 2, n - 2, MOD)
+        print(r%MOD)
+
+if __name__ == '__main__':
+    main()
